6289abd7d873daa2dd02fd1817de2e4fb6d31dd5
sqrtroot <robert.bezem@gmail.com>
2020-05-11 19:58:01 +0200

    Run install as sudo

--- .github/workflows/install_latest_lcov.sh
+++ .github/workflows/install_latest_lcov.sh
@@ -3,4 +3,4 @@ sudo apt install libjson-perl libperlio-gzip-perl
 directory=`mktemp /tmp/LCOV-XXXXXX -d`
 git clone https://github.com/linux-test-project/lcov.git $directory 
 cd $directory
-make install
+sudo make install
