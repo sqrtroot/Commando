3219d5ec770ed74653ccaf8ad75b1ee9ddb29043
sqrtroot <robert.bezem@gmail.com>
2020-05-11 21:22:40 +0200

    Revert "Made functions references, so clang can look trough them and optimize them."
    It worked in https://www.godbolt.org/z/_Uxk-Y but not in this instance.
    This reverts commit 339e0d5f06c95cfe9da068314665b1c180ecc4e6.

--- include/Commando/Shell.h
+++ include/Commando/Shell.h
@@ -14,18 +14,18 @@ struct Shell {
   const char * remove_char = "\b \b";
   std::string                              buffer;
   CommandHandlerBase &                     commands;
-  const std::function<bool()>&                    character_ready;
-  const std::function<char()>&                  get_char;
-  const std::function<void(nonstd::string_view)>& write_str;
+  const std::function<bool()>                    character_ready;
+  const std::function<char()>                    get_char;
+  const std::function<void(nonstd::string_view)> write_str;
 
   Shell(CommandHandlerBase &               commands,
-        const std::function<bool()>&                    character_ready,
-        const std::function<char()> &                   get_character,
-        const std::function<void(nonstd::string_view)>& write_str):
+        std::function<bool()>                    character_ready,
+        std::function<char()>                    get_character,
+        std::function<void(nonstd::string_view)> write_str):
       commands(commands),
-      character_ready(character_ready),
-      get_char(get_character),
-      write_str(write_str) {}
+      character_ready(std::move(character_ready)),
+      get_char(std::move(get_character)),
+      write_str(std::move(write_str)) {}
 
   void exec() {
     commands.handle_input(buffer);
