fc7492d163d4ad95c589369acc6fda036669bce4
sqrtroot <robert.bezem@gmail.com>
2020-05-16 15:10:10 +0200

    Fixed install scripts
    install latest version of iwyu, moved install clang to seperate script

--- .github/workflows/install_include-what-you-use.sh
+++ .github/workflows/install_include-what-you-use.sh
@@ -1,10 +1,10 @@
 set -e
-sudo apt install llvm-8-dev libclang-8-dev
+sudo apt install llvm-10-dev libclang-10-dev
 git clone https://github.com/include-what-you-use/include-what-you-use.git /iwyu
 cd /iwyu
-git checkout clang_8.0
+git checkout clang_10
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=/usr/lib/llvm-10/lib/cmake/
 make -j "$(nproc)"
 make install
